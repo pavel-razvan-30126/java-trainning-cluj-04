44a0449e64afa4f43421ee36c8d53be8f4c02ec9
Pavel Razvan <pykk1@mail.student.utcluj.ro>
2020-05-10 01:46:11 +0300

    Message formatted

--- _4_exceptions_&_IO/_4.2_secret_message/src/Secret_Message/Main.java
+++ _4_exceptions_&_IO/_4.2_secret_message/src/Secret_Message/Main.java
@@ -10,18 +10,15 @@ public class Main {
         File file = new File("D:\\UTCN\\Google\\java-trainning-cluj-04\\_4_exceptions_&_IO\\_4.2_secret_message\\src\\Secret_Message\\message.txt");
         FileReader fileReader = new FileReader(file);
         int c = fileReader.read();
-        while(c!=-1){
-            if(c>64 && c<91 && c!=88) //Between A and Z & !=X
+        while (c != -1) {
+            if (c > 64 && c < 91 && c != 88) //Between A and Z & !=X
             {
-                System.out.print(String.valueOf((char)c));
+                System.out.print(String.valueOf((char) c));
             }
-            if(c == 88){
+            if (c == 88) {
                 System.out.print(" ");
             }
             c = fileReader.read();
         }
-
-
-
     }
 }
